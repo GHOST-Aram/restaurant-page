d3f7bb9eaf0a1dcc9371bf1ad3db08a63734111b
GHOST-Aram <owinoojala@gmail.com>
2023-03-01 17:44:57 +0300

    Install and configure webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,17 @@ module.exports = {
             }
         ]
         
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: '3000',
+        hot: true,
+        open: true,
+        compress: true,
+        historyApiFallback: true,
     }
     
 }
