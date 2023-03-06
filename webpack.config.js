35477d98fb236693bbd8cbf75d5a5358a8b072a9
GHOST-Aram <owinoojala@gmail.com>
2023-03-06 16:33:21 +0300

    chore: Create new entry point in main.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const  HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'development',
     entry: {
-        bundle:'./src/scripts/index.js',
+        bundle:'./src/scripts/main.js',
        
     },
     output: {
